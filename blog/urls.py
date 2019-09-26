d8b22b094e90826120ad7ab5ebd3b977e170725c
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-09-26 19:12:26 +0700

    Kteam - Lesson 19: Comment in Python Django - Final Lesson

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='blog'),
-    path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('', views.PostListView, name='blog'),
+    path('<int:pk>/', views.post, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Comment
+from django.http import HttpResponseRedirect
+from blog.forms import CommentForm
 
-# Create your views here.
-#def list(request):
-#    Data = {'Posts': Post.objects.all().order_by("-date")}
-#    return render(request, 'blog/blog.html', Data) 
+#Create your views here.
+def PostListView(request):
+    Data = {'Posts': Post.objects.all().order_by("-date")}
+    return render(request, 'blog/blog.html', Data) 
 
-class PostListView(ListView):
-    ListView.queryset = Post.objects.all().order_by("-date")
-    template_name = 'blog/blog.html'
-    context_object_name = 'Posts'
-    paginate_by = 1
+#class PostListView(ListView):
+#    ListView.queryset = Post.objects.all().order_by("-date")
+#    template_name = 'blog/blog.html'
+#    context_object_name = 'Posts'
+#    paginate_by = 1
 
-#def post_detail(request, id):
-#    post = Post.objects.get(id=id)
-#    return render(request, 'blog/post.html', {'post': post})
+def post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST, author=request.user, post=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path)
+    return render(request, 'blog/post.html', {"post":post, "form":form})
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post.html'
+#class PostDetailView(DetailView):
+#    model = Post
+#    template_name = 'blog/post.html'
