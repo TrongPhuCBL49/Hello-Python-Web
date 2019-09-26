b1ccfaddb06038da22656acaf39010bc51f75aee
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-09-26 15:09:02 +0700

    Kteam - Lesson 16: Create Register form in Python Django

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register')
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .form import RegistrationForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     return render(request,'pages/home.html')
 def contact(request):
     return render(request,'pages/contact.html')
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'pages/register.html', {'form': form})
+
 def error_404(request):
     data = {}
     return render(request,'pages/error.html', data)
